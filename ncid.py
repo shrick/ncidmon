92e0ad1b748b77e0c7b7d94ef16d708f1771c4b0
shrick <sickhickrick@women-at-work.org>
2014-01-19 23:30:22 +0100

    implified code (less fucntions)

--- ncid.py
+++ ncid.py
@@ -11,63 +11,53 @@ SOCKET_TIMEOUT  = 1.0           # receive timeout in seconds
 CLIENT_MESSAGE  = "ncid.py client connected at %s" % (datetime.today())
 
 
-def get_sortable_date_time(date, time):
-    return datetime.strptime('%s %s' % (date, time),
+def get_sortable_entry_key(entry):
+    return datetime.strptime(
+        '%s %s' % (entry['DATE'], entry['TIME']),
         '%d%m%Y %H%M'
     ).strftime(
         '%Y-%m-%d %H:%M'
     )
-    
-def time_format(s):
-    return datetime.strptime(s, '%H%M').strftime('%H:%M')
-
-def date_format(s):
-    return datetime.strptime(s, '%d%m%Y').strftime('%d.%m.%Y')
 
 
+# requesting server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print "[DEBUG] connecting to NCID server '%s:%s'..." % (NCID_SERVER, NCID_PORT)
 s.connect((NCID_SERVER, NCID_PORT))
 print "[DEBUG] broadcasting myself..."
 s.send("MSG: " + CLIENT_MESSAGE)
+print "[DEBUG] reading responses with timeout of %s seconds..." % (SOCKET_TIMEOUT)
 s.settimeout(SOCKET_TIMEOUT)
 server_text = ""
-
-print "[DEBUG] reading responses with timeout of %s seconds..." % (SOCKET_TIMEOUT)
 try:
     data = s.recv(BUFFER_SIZE)
     while data:
         server_text += data
         data = s.recv(BUFFER_SIZE)
 except IOError as e:
-    print "[DEBUG] exception caught:", e
+    print "[DEBUG] exception:", e
     s.close()
 
-print "[DEBUG] formatted response follows..."
-print
 # collect log entries, output other lines
+print "[DEBUG] formatted response follows...\n"
 server_lines = server_text.split('\r\n')
-log_entries = {}
+log_entries = []
 for line in server_lines:
     if line.startswith('CIDLOG:'):
         # log entry
-        log_list = line.split('*')[1:]  # throw away 'CIDLOG: '
-        log_dict = dict(zip(*[iter(log_list)] * 2))
-        date = log_dict.get('DATE', 'yyyymmdd')
-        time = log_dict.get('TIME', 'hhmm')
-        key = get_sortable_date_time(date, time)
-        log_entries.update({key: log_dict})
+        log = line.split('*')[1:]  # throw away 'CIDLOG: ' element
+        entry = dict(zip(*[iter(log)] * 2))
+        log_entries.append(entry)
     elif line != "":
         print line  # other than log entry
 
-# formatted output of log entries
-for index, key in enumerate(sorted(log_entries)):
-    entry = log_entries[key]
+
+# formatted sorted output of log entries
+for index, entry in enumerate(sorted(log_entries, key=get_sortable_entry_key)):
     print "[{:02}]  {}, {}: {}".format(
         index + 1,
-        date_format(entry.get('DATE', 'yyyymmdd')), 
-        time_format(entry.get('TIME', 'hhmm')), 
+        datetime.strptime(entry['DATE'], '%d%m%Y').strftime('%d.%m.%Y') ,     
+        datetime.strptime(entry['TIME'], '%H%M').strftime('%H:%M'),
         entry.get('NMBR', '<missing>')
     )
-#    for k, v in entry.iteritems():
-#        print "  {}: {}".format(k, v)
+
